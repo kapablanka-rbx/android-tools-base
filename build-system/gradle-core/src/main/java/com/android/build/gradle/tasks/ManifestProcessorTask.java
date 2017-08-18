kapablanka-rbx/android-tools-base

commit c31c98fdf84790d57413e3065916bfd67cd14993

Reintroduced getManifestOutputFile API to manifest tasks.
The API was removed during the split/feature work as the manifest
tasks are now using an output folder which may contain 1 to many
manifest files.

This caused some user's scripts to fail without much information
on what to do next.

With this change, an exception will be raised providing a link
to the migration guide.

Bug=64021986
Test: added unit test

Change-Id: If8df6c1e762acee49b6b2a1451ec76bc5b68139d

build-system/gradle-core/src/main/java/com/android/build/gradle/tasks/ManifestProcessorTask.java
@@ -98,4 +98,20 @@ public abstract class ManifestProcessorTask extends IncrementalTask {
                         mapToSerialize.entrySet(),
                         (input) -> keyValueJoiner.join(input.getKey(), input.getValue()))));
     }
+
+    /**
+     * Backward compatibility support. This method used to be available on AGP < 3.0 but has now
+     * been replaced with {@link #getManifestOutputDirectory()}.
+     *
+     * @return
+     * @deprecated As or release 3.0, replaced with {@link #getManifestOutputDirectory()}
+     */
+    @Deprecated
+    @Internal
+    public File getManifestOutputFile() {
+        throw new RuntimeException(
+                "Manifest Tasks does not support the manifestOutputFile property any more, please"
+                        + " use the manifestOutputDirectory instead.\nFor more information, please check "
+                        + "https://developer.android.com/studio/build/gradle-plugin-3-0-0-migration.html");
+    }
 }

build-system/gradle-core/src/test/java/com/android/build/gradle/tasks/ProcessManifestTest.kt
@@ -1,0 +1,58 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.build.gradle.tasks
+
+import com.google.common.truth.Truth
+import org.gradle.testfixtures.ProjectBuilder
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.rules.TemporaryFolder
+import org.mockito.MockitoAnnotations
+import java.io.IOException
+
+/**
+ * Tests for {@link ProcessManifest}
+ */
+class ProcessManifestTest {
+
+    @Rule @JvmField var temporaryFolder = TemporaryFolder()
+
+    internal lateinit var task: ProcessManifest
+
+    @Before
+    @Throws(IOException::class)
+    fun setUp() {
+
+        MockitoAnnotations.initMocks(this)
+        val project = ProjectBuilder.builder().withProjectDir(temporaryFolder.root).build()
+
+        task = project!!.tasks.create("processManifest", ProcessManifest::class.java)
+    }
+
+    @Test()
+    fun testBackwardCompatibility() {
+        try {
+            @Suppress("DEPRECATION")
+            task.manifestOutputFile
+            Assert.fail("failed to raise backward incompatible exception")
+        } catch(e: RuntimeException) {
+            Truth.assertThat(e.message).contains("gradle-plugin-3-0-0-migration.html");
+        }
+    }
+}

commit 0190001e1e271fbbeefe163b679f7ea1fcbe4203

Fix PSQ errors caused by invalid JavaDoc.
Fix the malformed HTML errors in the presubmit queue, introduced
by c31c98fdf84790d57413e3065916bfd67cd14993.

Test: Verified that the presubmit queue passed.
Change-Id: If87cf300fbf7f785e9f1ec7f04bb83315402e77e

build-system/gradle-core/src/main/java/com/android/build/gradle/tasks/ManifestProcessorTask.java
@@ -100,8 +100,8 @@ public abstract class ManifestProcessorTask extends IncrementalTask {
     }
 
     /**
-     * Backward compatibility support. This method used to be available on AGP < 3.0 but has now
-     * been replaced with {@link #getManifestOutputDirectory()}.
+     * Backward compatibility support. This method used to be available on AGP prior to 3.0 but has
+     * now been replaced with {@link #getManifestOutputDirectory()}.
      *
      * @return
      * @deprecated As or release 3.0, replaced with {@link #getManifestOutputDirectory()}