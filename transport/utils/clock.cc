kapablanka-rbx/android-tools-base

commit 31a082a08014f81532097951cca813e21a237c6d

Minor refactor: removing int64->double->int64 conversion.
Test: manual
Bug: none

Change-Id: I39df76c4dcd3592003256c7bb1ea897174894161

transport/utils/clock.cc
@@ -22,7 +22,7 @@ namespace profiler {
 int64_t SteadyClock::GetCurrentTime() const {
   timespec time;
   clock_gettime(CLOCK_MONOTONIC, &time);
-  return 1e9 * time.tv_sec + time.tv_nsec;
+  return 1000000000 * time.tv_sec + time.tv_nsec;
 }
 
 }  // namespace profiler

commit fc227628f5df3503cf84ccf2e56e74612d5f8ebc

Simplify timestamp calculation
I wanted to write a regression test for b/130211086,
but it's hard without complicating production code.
The code that caused the problem was simple, but had
some C++ language implication that's not obvious.
Therefore I rewrite the production code with a tested
function and (possibly verbose) documentation.

The bug was caused by commit 31a082a08014f81532097951cca813e21a237c6d,
fixed by commit 7b424bfed051d69853ab30c1211a277721000a56.
Hopefully this CL will make it less likely to break if
someone refactors it in the future.

Bug: 130211086
Test: N/A; new implementation uses an already-tested
utility function, and new logic is too simple to test.

Change-Id: I2892b06941af48fa8e9261edbed7559a4977e01d

transport/utils/clock.cc
@@ -22,7 +22,31 @@ namespace profiler {
 int64_t SteadyClock::GetCurrentTime() const {
   timespec time;
   clock_gettime(CLOCK_MONOTONIC, &time);
-  return static_cast<int64_t>(1000000000) * time.tv_sec + time.tv_nsec;
+
+  // time.tv_sec is of type 'time_t'; time.tv_nsec is of type 'long'.
+  //
+  // On a 32-bit device, they are both 4 bytes long. As a result, simplily
+  // writing
+  //     1000000000 * time.tv_sec + time.tv_nsec
+  // in C++ would easily cause integer overflow because C++ would save the
+  // multiplication result into a 32-bit integer. The overflow happens roughly
+  // every 2.2 seconds.
+  // Math: (2 ^ 31 -1) / 1000000000 ~= 2.15
+  //
+  // On a 64-bit device, time_t and long are both 8 bytes long.
+  // Consider 'time.tv_sec' as in seconds held by a 64-bit integer. When the
+  // unit changes to nanoseconds, the number becomes a billion times larger, and
+  // the new value might be too large for a 64-bit integer to hold in principle.
+  // However, in practice, Android devices reset CLOCK_MONOTONIC every time when
+  // it reboots. In order to overflow, the device needs to be run 292 years.
+  // Math: (2 ^ 63 - 1) / 1000000000 / 60 / 60 / 24 / 365 ~= 292.47
+  //
+  // Therefore, it's reasonable for our purpose to use int64_t to hold the
+  // timestamp in nanosecond unit. Clock::s_to_ns() has the right input and
+  // output types, and it works on both 32-bit and 64-bit platforms. Its
+  // signature is as follows. Note constexpr implies inline.
+  //   static constexpr int64_t s_to_ns(int64_t s);
+  return Clock::s_to_ns(time.tv_sec) + time.tv_nsec;
 }
 
 }  // namespace profiler