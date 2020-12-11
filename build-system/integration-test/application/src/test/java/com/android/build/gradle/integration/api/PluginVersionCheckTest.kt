kapablanka-rbx/android-tools-base

commit 241ac79154ef2f85557fca2918294dba6622f62a

Apply min plugin version check only to current subproject
Other subprojects that do not apply the Android Gradle plugin should not
be impacted by this check.

Bug: 148776286
Test: Existing GradlePluginUtilsTest, PluginVersionCheckTest
Change-Id: I8a85c272b7d230e00bce4f5d36670e5f299fd098

build-system/gradle-core/src/main/java/com/android/build/gradle/internal/utils/GradlePluginUtils.kt
@@ -26,9 +26,6 @@ import org.gradle.api.artifacts.result.DependencyResult
 import org.gradle.api.artifacts.result.ResolvedDependencyResult
 import org.gradle.api.initialization.dsl.ScriptHandler.CLASSPATH_CONFIGURATION
 
-private const val INTERNAL__CHECKED_MINIMUM_PLUGIN_VERSIONS =
-    "INTERNAL__CHECKED_MINIMUM_PLUGIN_VERSIONS"
-
 private val pluginList = listOf(
     /**
      *  https://issuetracker.google.com/116747159
@@ -78,26 +75,11 @@ internal data class DependencyInfo(
  * Enforces minimum versions of certain plugins.
  */
 fun enforceMinimumVersionsOfPlugins(project: Project, issueReporter: IssueReporter) {
-    // We're going to check all projects at the end of the configuration phase, so make sure to do
-    // this check only once by marking a custom property of the root project. The access doesn't
-    // need to be thread-safe as configuration is single-threaded.
-    val alreadyChecked =
-        project.rootProject.extensions.extraProperties.has(
-            INTERNAL__CHECKED_MINIMUM_PLUGIN_VERSIONS
-        )
-    if (alreadyChecked) {
-        return
-    }
-    project.rootProject.extensions.extraProperties.set(
-        INTERNAL__CHECKED_MINIMUM_PLUGIN_VERSIONS,
-        true
-    )
-
-    project.gradle.projectsEvaluated { gradle ->
-        gradle.allprojects {
-            for (plugin in pluginList) {
-                enforceMinimumVersionOfPlugin(it, plugin, issueReporter)
-            }
+    // Apply this check only to current subproject, other subprojects that do not apply the Android
+    // Gradle plugin should not be impacted by this check (see bug 148776286).
+    project.afterEvaluate {
+        for (plugin in pluginList) {
+            enforceMinimumVersionOfPlugin(it, plugin, issueReporter)
         }
     }
 }
@@ -154,7 +136,7 @@ internal fun visitDependency(
 
     // The selected dependency may be different from the requested dependency, but we are interested
     // in only the selected dependency
-    val dependency = (dependencyResult as ResolvedDependencyResult).selected
+    val dependency = dependencyResult.selected
     val moduleVersion = dependency.moduleVersion!!
     val group = moduleVersion.group
     val name = moduleVersion.name

commit 28ed32b344e156ddeed06e2f66defe5e544ab5cd

Check parent projects when resolving buildscript classpath
In GradlePluginUtils.kt, when resolving a buildscript classpath of a
project, we should also check its parent projects recursively because
the buildscript classpath(s) of parent projects are available to child
projects.

This regression was introduced in commit 241ac79 (AGP 4.1).

Bug: 148776286
Test: Updated PluginVersionCheckTest
Change-Id: I6de10a8e6da11cc1d54e799a36967f070f14e0d4

build-system/gradle-core/src/main/java/com/android/build/gradle/internal/utils/GradlePluginUtils.kt
@@ -76,11 +76,34 @@ internal data class DependencyInfo(
  * Enforces minimum versions of certain plugins.
  */
 fun enforceMinimumVersionsOfPlugins(project: Project, issueReporter: IssueReporter) {
-    // Apply this check only to current subproject, other subprojects that do not apply the Android
-    // Gradle plugin should not be impacted by this check (see bug 148776286).
-    project.afterEvaluate {
-        for (plugin in pluginList) {
-            enforceMinimumVersionOfPlugin(it, plugin, issueReporter)
+    // Run only once per build
+    val extraProperties = project.rootProject.extensions.extraProperties
+    if (extraProperties.has(AGP_INTERNAL__MIN_PLUGIN_VERSION_CHECK_STARTED)) {
+        return
+    }
+    extraProperties.set(AGP_INTERNAL__MIN_PLUGIN_VERSION_CHECK_STARTED, true)
+
+    project.gradle.projectsEvaluated { gradle ->
+        val projectsToCheck = mutableSetOf<Project>()
+        gradle.allprojects {
+            // Check only projects that have AGP applied (see bug 148776286).
+            // Also check their parent projects recursively because the buildscript classpath(s) of
+            // parent projects are available to child projects.
+            if (it.pluginManager.hasPlugin(ANDROID_GRADLE_PLUGIN_ID)) {
+                var current: Project? = it
+                while (current != null && projectsToCheck.add(current)) {
+                    current = current.parent
+                }
+            }
+        }
+        // Calling allprojects again is needed as Gradle doesn't allow cross-project resolution of
+        // buildscript classpath
+        gradle.allprojects {
+            if (it in projectsToCheck) {
+                for (pluginToCheck in pluginList) {
+                    enforceMinimumVersionOfPlugin(it, pluginToCheck, issueReporter)
+                }
+            }
         }
     }
 }
@@ -134,11 +157,14 @@ internal class ViolatingPluginDetector(
                     dep.moduleVersion?.let { "${it.group}:${it.name}:${it.version}" }
                             ?: dep.toString()
                 }
+                // Ignore Safe-args plugin for now (bug 175379963).
+                if (dependencyPath.contains("androidx.navigation:navigation-safe-args-gradle-plugin:2.3.1")) {
+                    return
+                }
                 // The root of the dependency graph (the start of the dependency path) in this case
                 // is not in a user-friendly format (e.g., ":<project-name>:unspecified"), so we use
                 // `project.displayName` instead (e.g., "root project '<project-name>'").
-                val adjustedPath = listOf(projectDisplayName) +
-                        dependencyPath.let { it.subList(1, it.size) }
+                val adjustedPath = listOf(projectDisplayName) + dependencyPath.drop(1)
                 pathsToViolatingPlugins.add(adjustedPath.joinToString(" -> "))
             }
         }
@@ -178,3 +204,5 @@ fun getBuildSrcPlugins(classLoader: ClassLoader): Set<String> {
     }
     return buildSrcPlugins
 }
+
+private const val AGP_INTERNAL__MIN_PLUGIN_VERSION_CHECK_STARTED = "AGP_INTERNAL__MIN_PLUGIN_VERSION_CHECK_STARTED"

build-system/gradle-core/src/main/java/com/android/build/gradle/internal/utils/agpVersionChecker.kt
@@ -61,7 +61,7 @@ private fun compareVersions(
     firstVersion: String,
     projectToCheck: Project
 ) {
-    projectToCheck.plugins.withId("com.android.base") {
+    projectToCheck.plugins.withId(ANDROID_GRADLE_PLUGIN_ID) {
         val versionValue = try {
             val versionClass = try {
                 it::class.java.classLoader.loadClass(com.android.Version::class.java.name)
@@ -91,4 +91,5 @@ Using multiple versions of the Android Gradle plugin in the same build is not al
     }
 }
 
+const val ANDROID_GRADLE_PLUGIN_ID = "com.android.base"
 private const val CHECK_PERFORMED = "android.agp.version.check.performed"

build-system/gradle-core/src/test/java/com/android/build/gradle/api/AppliedPluginTest.java
@@ -16,6 +16,7 @@
 
 package com.android.build.gradle.api;
 
+import static com.android.build.gradle.internal.utils.AgpVersionChecker.ANDROID_GRADLE_PLUGIN_ID;
 import static com.google.common.truth.Truth.assertThat;
 
 import com.android.build.gradle.internal.fixture.TestProjects;
@@ -67,6 +68,6 @@ public class AppliedPluginTest {
         assertThat(project.getPlugins().findPlugin(pluginName)).isNotNull();
         assertThat(project.getPlugins().findPlugin(pluginClass)).isNotNull();
         assertThat(project.getPlugins().findPlugin(AndroidBasePlugin.class)).isNotNull();
-        assertThat(project.getPlugins().findPlugin("com.android.base")).isNotNull();
+        assertThat(project.getPlugins().findPlugin(ANDROID_GRADLE_PLUGIN_ID)).isNotNull();
     }
 }

build-system/integration-test/application/src/test/java/com/android/build/gradle/integration/api/PluginVersionCheckTest.kt
@@ -18,7 +18,8 @@ package com.android.build.gradle.integration.api
 
 import com.android.build.gradle.integration.common.fixture.GradleTestProject
 import com.android.build.gradle.integration.common.fixture.ModelContainer
-import com.android.build.gradle.integration.common.fixture.app.HelloWorldApp
+import com.android.build.gradle.integration.common.fixture.app.MinimalSubProject
+import com.android.build.gradle.integration.common.fixture.app.MultiModuleTestProject
 import com.android.build.gradle.integration.common.truth.ScannerSubject
 import com.android.build.gradle.integration.common.utils.TestFileUtils
 import com.android.builder.model.AndroidProject
@@ -32,24 +33,28 @@ class PluginVersionCheckTest {
 
     @get:Rule
     val project = GradleTestProject.builder()
-        .fromTestApp(HelloWorldApp.forPlugin("com.android.library"))
-        .create()
+            .fromTestApp(
+                    MultiModuleTestProject.builder()
+                            .subproject("lib", MinimalSubProject.lib("com.example.lib"))
+                            .build()
+            ).create()
 
     @Test
     fun testButterKnifeTooOld() {
         // Use an old version of the ButterKnife plugin, expect sync issues
-        TestFileUtils.searchAndReplace(
+        TestFileUtils.appendToFile(
             project.buildFile,
-            "android {\n",
             """
                 buildscript {
                     dependencies {
                         classpath 'com.jakewharton:butterknife-gradle-plugin:9.0.0-rc1'
                     }
                 }
-                apply plugin: 'com.jakewharton.butterknife'
-                android {
                 """.trimIndent()
+        )
+        TestFileUtils.appendToFile(
+                project.getSubproject("lib").buildFile,
+                "apply plugin: 'com.jakewharton.butterknife'"
         )
 
         val model = project.model().ignoreSyncIssues().fetchAndroidProjects()
@@ -75,18 +80,19 @@ class PluginVersionCheckTest {
     @Test
     fun testButterKnifeOk() {
         // Use a sufficiently new version of the ButterKnife plugin, expect no sync issues
-        TestFileUtils.searchAndReplace(
+        TestFileUtils.appendToFile(
             project.buildFile,
-            "android {\n",
             """
                 buildscript {
                     dependencies {
                         classpath 'com.jakewharton:butterknife-gradle-plugin:9.0.0-rc2'
                     }
                 }
-                apply plugin: 'com.jakewharton.butterknife'
-                android {
                 """.trimIndent()
+        )
+        TestFileUtils.appendToFile(
+                project.getSubproject("lib").buildFile,
+                "apply plugin: 'com.jakewharton.butterknife'"
         )
 
         val model = project.model().fetchAndroidProjects()