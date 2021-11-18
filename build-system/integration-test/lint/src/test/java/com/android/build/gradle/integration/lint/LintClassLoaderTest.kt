Iandmee/base_tools

Share lint classloader even when buildSrc changes
As the underlying UAST machinery always ends up leaking the classloader,
this logic uses a MBean (via JvmWideVariable) to cache lint
classloaders, so that the class can be reused even when AGP is loaded in
a new classloader because of a buildSrc change.

The current caching was already problematic for -dev versions, but now
caching more keyed on the classpath URIs will likely cause unexpected
reusing when developing AGP and lint. To address this, the key is now
the explicit version, plus the hash of the files for -dev versions.

Isolate the lint invocation from class loaders leak by resetting
the thread context class loader before and after invocations.

Bug: 194911959
Test: Expanded LintClassLoaderTest
Change-Id: I804a2e3d9c6a27bc910c5c9cad6ea0d125f8c033

build-system/integration-test/lint/src/test/java/com/android/build/gradle/integration/lint/LintClassLoaderTest.kt
@@ -19,13 +19,18 @@ package com.android.build.gradle.integration.lint
 import com.google.common.truth.Truth.assertThat
 import org.junit.Rule
 import org.junit.Test
+import java.nio.file.Files
 import java.util.Scanner
 import java.util.regex.Pattern
 
 /**
- * Integration test to check that AGP isn't leaking lint class loaders.
+ * Integration test to check that AGP isn't leaking lint class loaders, even when buildSrc changes.
  */
 class LintClassLoaderTest {
+
+    companion object {
+        const val CREATING_LINT_CLASS_LOADER_LOG_LINE = "Android Lint: Creating lint class loader.*"
+    }
 
     // Set a unique heapSize for each test to ensure each test uses a fresh gradle daemon without an
     // existing lint class loader.
@@ -34,11 +39,32 @@ class LintClassLoaderTest {
 
     @Test
     fun testForSingleLintClassLoader() {
+        val buildSrcBuildGradle = project.file("buildSrc/build.gradle")
+        Files.createDirectory(buildSrcBuildGradle.toPath().parent)
+        buildSrcBuildGradle.writeText(//language=groovy
+            """
+            plugins {
+                id('java')
+            }
+        """.trimIndent())
+        val buildSrcClass = project.file("buildSrc/src/main/java/com/example/MyPlugin.java")
+        Files.createDirectories(buildSrcClass.toPath().parent)
+        buildSrcClass.writeText(//language=java
+            """
+            package com.example;
+
+            public class MyPlugin {
+                public static int myConstant = 1;
+            }
+        """.trimIndent())
+
         // Check that we create exactly one lint class loader when running several lint tasks with
         // a new gradle daemon.
-        project.executor().withArgument("--info").run("lintDebug", "lintRelease")
-        assertThat(project.buildResult.stdout.countMatches("Creating lint class loader."))
-            .isEqualTo(1)
+        val result = project.executor().withArgument("--info").run("lintDebug", "lintRelease")
+        assertThat(result.stdout.countMatches(CREATING_LINT_CLASS_LOADER_LOG_LINE)).isEqualTo(1)
+        buildSrcClass.writeText(buildSrcClass.readText().replace('1', '2'))
+        val result2 = project.executor().withArgument("--info").run("clean","lintDebug")
+        assertThat(result2.stdout.countMatches(CREATING_LINT_CLASS_LOADER_LOG_LINE)).isEqualTo(0)
     }
 }
 

kapablanka-rbx/android-tools-base

build-system/integration-test/lint/src/test/java/com/android/build/gradle/integration/lint/LintClassLoaderTest.kt
@@ -23,13 +23,14 @@ import com.android.build.gradle.options.BooleanOption
 import com.google.common.truth.Truth.assertThat
 import org.junit.Rule
 import org.junit.Test
+import java.nio.file.Files
 import org.junit.runner.RunWith
 import org.junit.runners.Parameterized
 import java.util.Scanner
 import java.util.regex.Pattern
 
 /**
- * Integration test to check that AGP isn't leaking lint class loaders.
+ * Integration test to check that AGP isn't leaking lint class loaders, even when buildSrc changes.
  */
 @RunWith(FilterableParameterized::class)
 class LintClassLoaderTest(private val usePartialAnalysis: Boolean) {
@@ -38,6 +39,7 @@ class LintClassLoaderTest(private val usePartialAnalysis: Boolean) {
         @Parameterized.Parameters(name = "usePartialAnalysis = {0}")
         @JvmStatic
         fun params() = listOf(true, false)
+        const val CREATING_LINT_CLASS_LOADER_LOG_LINE = "Android Lint: Creating lint class loader.*"
     }
 
     // Set a unique heapSize for each test to ensure each test uses a fresh gradle daemon without an
@@ -48,11 +50,32 @@ class LintClassLoaderTest(private val usePartialAnalysis: Boolean) {
 
     @Test
     fun testForSingleLintClassLoader() {
+        val buildSrcBuildGradle = project.file("buildSrc/build.gradle")
+        Files.createDirectory(buildSrcBuildGradle.toPath().parent)
+        buildSrcBuildGradle.writeText(//language=groovy
+            """
+            plugins {
+                id('java')
+            }
+        """.trimIndent())
+        val buildSrcClass = project.file("buildSrc/src/main/java/com/example/MyPlugin.java")
+        Files.createDirectories(buildSrcClass.toPath().parent)
+        buildSrcClass.writeText(//language=java
+            """
+            package com.example;
+
+            public class MyPlugin {
+                public static int myConstant = 1;
+            }
+        """.trimIndent())
+
         // Check that we create exactly one lint class loader when running several lint tasks with
         // a new gradle daemon.
-        project.getExecutor().withArgument("--info").run("lintDebug", "lintRelease")
-        assertThat(project.buildResult.stdout.countMatches("Creating lint class loader."))
-            .isEqualTo(1)
+        val result = project.getExecutor().withArgument("--info").run("lintDebug", "lintRelease")
+        assertThat(result.stdout.countMatches(CREATING_LINT_CLASS_LOADER_LOG_LINE)).isEqualTo(1)
+        buildSrcClass.writeText(buildSrcClass.readText().replace('1', '2'))
+        val result2 = project.executor().withArgument("--info").run("clean","lintDebug")
+        assertThat(result2.stdout.countMatches(CREATING_LINT_CLASS_LOADER_LOG_LINE)).isEqualTo(0)
     }
 
     private fun GradleTestProject.getExecutor(): GradleTaskExecutor =